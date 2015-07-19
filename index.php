531ec1c992089eaa24e600b1086fd92d94640c30
Paolo <tropaolo@gmail.com>
2015-07-19 19:31:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,31 @@
 <?php
+/**
+*  This file is a class of ReporterGUI
+*
+*  @author itpao25
+*  ReporterGUI Web Interface  Copyright (C) 2015
+*
+*  This program is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU General Public License as
+*  published by the Free Software Foundation; either version 2 of
+*  the License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  @package ReporterGUI
+*/
 
-	/* 	ReporterGUI Web Interface  Copyright (C) 2015  itpao25
+define("RG_ROOT_INDEX", "true", true);
 
-		This program is free software; you can redistribute it and/or
-		modify it under the terms of the GNU General Public License as
-		published by the Free Software Foundation; either version 2 of
-		the License, or (at your option) any later version.
+require_once("inc/heart.inc.php");
 
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-
-	*/
-
-	define("RG_ROOT_INDEX", "true", true);
-
-	require_once("inc/heart.inc.php");
-
-	if($RGWeb->isLogged()) {
-		$RGWeb->getHomeIndex();
-	} else {
-		$RGWeb->getLoginIndex();
-	}
+if($RGWeb->isLogged()) {
+	$RGWeb->getHomeIndex();
+} else {
+	$RGWeb->getLoginIndex();
+}
 
 ?>

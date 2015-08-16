e1a120218e932d4d4d84c802ebe5aca957a5d6a8
Paolo <tropaolo@gmail.com>
2015-08-16 04:38:55 +0200

    Update heart.inc.php

--- inc/heart.inc.php
+++ inc/heart.inc.php
@@ -834,6 +834,8 @@ Class ReporterGUI
 		}
 
 		$name = trim(mysqli_real_escape_string($this->mysqli, $this->escape_html($name)));
+		$name = $this->getUtily->clean($name);
+		
 		$password = trim(mysqli_real_escape_string($this->mysqli, $this->escape_html($password)));
 		$permission = trim(mysqli_real_escape_string($this->mysqli, $this->escape_html($permission)));
 		if( strlen($name) >= 1 || strlen($password) >= 1 )
